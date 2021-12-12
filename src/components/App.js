9f515b843c843fd923508d72b7e3c63d9a576587
mikaeljohnson <mikaeljohnsoncrypto@gmail.com>
2021-12-12 06:24:58 -0500

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
 import './App.css';
 import Web3 from 'web3';
-import VInterface from '../abis/VInterface.json'
-import Vesting from '../abis/Vesting.json'
 import IERC20 from '../abis/IERC20.json'
-import styled from "styled-components";
-import gif from '../2ggwt.gif';
-
-
-
+import Presale from '../abis/BlaqPresale.json'
+import gif from '../gif.gif';
+import * as s from "../styles/globalStyles";
+import GaugeChart from 'react-gauge-chart'
+import { Container } from 'react-bootstrap';
 
 class App extends Component {
   
@@ -38,37 +35,102 @@ class App extends Component {
     this.setState({ account: accounts[0] })
     // Network ID
     const networkId = await web3.eth.net.getId()
-    const networkData = 137 == networkId
+    const networkData = 137 === networkId
     if(networkData) {
-      const oldVest = new web3.eth.Contract(VInterface.abi, "0x1DBE25868679EDe8Eb93fED1138dc3d19ABb74B1")
-      this.setState({oldVest})
-      const newVest = new web3.eth.Contract(Vesting.abi, "0xce7695743698d8e7635CC973568D8f70a45a1729")
-      this.setState({newVest})
-      const token = new web3.eth.Contract(IERC20.abi, "0xc08e94e12ca1357DF36F3c16c3A1df5F84c7B801")
-      this.setState({token})
-      const appValue = await newVest.methods.beneficiaryBalance(accounts[0]).call()
+      const presale = new web3.eth.Contract(Presale.abi, "0x28A58961A439DDd49a07Ca87B46e0039e77f7D2c")
+      this.setState({presale})
+      const usdc = new web3.eth.Contract(IERC20.abi, "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174")
+      this.setState({usdc})
+      const usdt = new web3.eth.Contract(IERC20.abi, "0xc2132D05D31c914a87C6611C10748AEb04B58e8F")
+      this.setState({usdt})
+      const wmatic = new web3.eth.Contract(IERC20.abi, "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270")
+      this.setState({wmatic})
+      const appValue = "10000000000000000000000000000000000000000000000000000000000000000";
       this.setState({appValue})
-      console.log(appValue)
+
+      var tokensSold = 0;
+      await this.state.presale.methods._tokensSold().call({from: this.state.account}).then(function(result){
+        tokensSold = result;
+      })
+      this.setState({tokensSold})
+
+      var currentRound = 0;
+      await this.state.presale.methods._currentRound().call({from: this.state.account}).then(function(result){
+        currentRound = result;
+      })
+      this.setState({currentRound})
+
+      var leftInRound = 0;
+      await this.state.presale.methods._rounds(this.state.currentRound).call({from: this.state.account}).then(function(result){
+        leftInRound = result._tokensLeft
+      })
+      this.setState({leftInRound})
+      
+      var leftInRoundPercent = leftInRound / 1250
+      this.setState({leftInRoundPercent})
+
+      var userVested = 1;
+      await this.state.presale.methods.viewVestedTokens(this.state.account).call({from: this.state.account}).then(function(result){
+        userVested = web3.utils.fromWei(result, 'ether')
+      })
+      this.setState({userVested})
+
+      var usdcApproved = 0;
+      await this.state.usdc.methods.allowance(this.state.account, "0x28A58961A439DDd49a07Ca87B46e0039e77f7D2c").call().then(function(result){
+        usdcApproved = web3.utils.fromWei(result, 'ether')
+      })
+      this.setState({usdcApproved})
+      
+      var usdtApproved = 0;
+      await this.state.usdt.methods.allowance(this.state.account, "0x28A58961A439DDd49a07Ca87B46e0039e77f7D2c").call().then(function(result){
+        usdtApproved = web3.utils.fromWei(result, 'ether')
+      })
+      this.setState({usdtApproved})
+     
+      var wmaticApproved = 0;
+      await this.state.wmatic.methods.allowance(this.state.account, "0x28A58961A439DDd49a07Ca87B46e0039e77f7D2c").call().then(function(result){
+        wmaticApproved = web3.utils.fromWei(result, 'ether')
+      })
+      this.setState({wmaticApproved})
+     
     } else {
       window.alert('You are not connected to the Polygon network.')
     }
   }
 
-  claimAndApprove = description => {
-    console.log("Claiming And Approving")
-    console.log(this.state.appValue)
-
-    this.state.oldVest.methods.claim().send({ from: this.state.account })
-    console.log(this.state.appValue)
-
-    this.state.token.methods.approve("0xce7695743698d8e7635CC973568D8f70a45a1729", this.state.appValue).send({ from: this.state.account })
-    console.log(this.state.appValue)
-
+  approveUsdc = description => {
+    console.log("Approving USDC")
+    this.state.usdc.methods.approve("0x28A58961A439DDd49a07Ca87B46e0039e77f7D2c", this.state.appValue).send({ from: this.state.account })
   }
 
-  claimNFT = description => {
-    console.log("Claiming NFT")
-    this.state.newVest.methods.claimNFT().send({ from: this.state.account })
+  approveUsdt = description => {
+    console.log("Approving USDT")
+    this.state.usdt.methods.approve("0x28A58961A439DDd49a07Ca87B46e0039e77f7D2c", this.state.appValue).send({ from: this.state.account })
+  }
+
+  approveWMatic = description => {
+    console.log("Approving WMatic")
+    this.state.wmatic.methods.approve("0x28A58961A439DDd49a07Ca87B46e0039e77f7D2c", this.state.appValue).send({ from: this.state.account })
+  }
+
+  buyUsdc = description => {
+    console.log(this.state.message)
+    this.state.presale.methods.buyWithUsdc(this.state.message).send({from: this.state.account})
+
+  }
+  
+  buyUsdt = description => {
+    console.log(this.state.appValue)
+    this.state.presale.methods.buyWithUsdt(this.state.message).send({from: this.state.account})
+  }
+
+  buyWMatic = description => {
+    console.log(this.state.appValue)
+    this.state.presale.methods.buyWithWMatic(this.state.message).send({from: this.state.account})
+  }
+
+  claim = description => {
+    this.state.presale.methods.claim().send({from: this.state.account})
   }
 
   constructor(props) {
@@ -76,62 +138,160 @@ class App extends Component {
     this.state = {
       account: '',
       loading: true,
-      appValue: 0
+      appValue: 0,
+      message: 0
     }
 
-    this.claimNFT = this.claimNFT.bind(this)
-    this.claimAndApprove = this.claimAndApprove.bind(this)
+    this.claim = this.claim.bind(this)
+    this.approveUsdc = this.approveUsdc.bind(this)
+    this.approveUsdt = this.approveUsdt.bind(this)
+    this.approveWMatic = this.approveWMatic.bind(this)
+    this.buyUsdc = this.buyUsdc.bind(this)
+    this.buyUsdt = this.buyUsdt.bind(this)
+    this.buyWMatic = this.buyWMatic.bind(this)
 
+  }
+  state = {message: '3'};
+
+  updateNumber = (e) => {
+    const val = e.target.value;
+    // If the current value passes the validity test then apply that to state
+    if (e.target.validity.valid) this.setState({message: e.target.value}); 
+    // If the current val is just the negation sign, or it's been provided an empty string,
+    // then apply that value to state - we still have to validate this input before processing
+    // it to some other component or data structure, but it frees up our input the way a user
+    // would expect to interact with this component
+    else if (val === '' || val === '-') this.setState({message: val});
   }
   render() {
     return (
-      <div>
+      
+      <div  className='App'>
+      
+      <div >
+        
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href=""
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Gigawatt Founders NFT Launch
+            BLAQ Presale
           </a>
+          
         </nav>
+        <center>
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 text-center">
               
               <div className="content mr-auto ml-auto">
                
                   <img src={gif} className="App-logo" alt="logo" />
-               
+                  
                 
-                <h1>Gigawatt Founders NFTs</h1>
+                <h1>Blaq Token Presale</h1>
                 <p>
-                  Follow the buttons to receive your Founders NFT, please only click button 2 after confirming the first 2 transactions
+                  Purchase Blaq Tokens for USDC, USDT or wMatic
                 </p>
-                <button       style={{height: '30px', width : '200px'}}
 
-
+                  
+                <s.SpacerXSmall/>
+                <p>
+                  You have purchased {this.state.userVested} tokens that are currently vesting.
+                </p>
+                <input
+                    type='tel'
+                    placeholder="0"
+                    value={this.state.message}
+                    onChange={this.updateNumber}
+                    pattern="^-?[0-9]\d*\.?\d*$"
+                    />
+                  <s.SpacerXSmall />
+                <button  className='buy-button'  style={{height: '50px', width : '200px'}}
+                      
                       onClick={(e) => {
                         e.preventDefault();
-                        this.claimAndApprove();
-                        
+                        if(parseInt(this.state.usdcApproved) > 0) {
+                          
+                          this.buyUsdc();
+                        } else {
+                          this.approveUsdc();
+                        }
                       }}
-                      ></button> 
-                
-                    <button     style={{height: '30px', width : '200px'}}
-
+                      >Buy with USDC</button> 
+                <button  className='buy-button'     style={{height: '50px', width : '200px'}}
                       onClick={(e) => {
                         e.preventDefault();
-                        this.claimNFT();
-                        
+                        if(parseInt(this.state.usdtApproved) > 0) {
+                          this.buyUsdt();
+                        } else {
+                          this.approveUsdt();
+                        }
                       }}
-                      ></button> 
+                      >Buy with USDT</button> 
+                <button  className='buy-button'     style={{height: '50px', width : '200px'}}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        if(parseInt(this.state.wmaticApproved) > 0) {
+                          this.buyWMatic();
+                        } else {
+                          this.approveWMatic();
+                        }
+                      }}
+                      >Buy with wMatic</button>   
+                <s.SpacerXSmall/>  
               </div>
-              
             </main>
+
+            <Container className='gaugeArea'>
+                <Container className='gaugeInfo'>
+                <p>
+                  {this.state.leftInRound} Current Round Sales
+                </p>
+
+                <GaugeChart className='gauge' id="gauge-chart2" 
+                  nrOfLevels={20} 
+                      
+                
+                  percent={this.state.leftInRoundPercent}
+                
+                />
+
+                </Container>
+                
+                <Container className='gaugeInfo'>
+                <p>
+                  {this.state.tokensSold} Blaq Tokens Sold so Far!
+                </p>
+
+                <GaugeChart className='gauge' id="gauge-chart2" 
+                 nrOfLevels={20} 
+                percent={this.state.tokensSold/5000} />
+
+                </Container>
+
+                <Container className='gaugeInfo'>
+                <p>
+                  Round {this.state.currentRound} 
+                </p>
+
+                <GaugeChart className='gauge' id="gauge-chart2" 
+                 nrOfLevels={20} 
+                percent={(this.state.currentRound-1)*0.25} />
+                
+
+
+                </Container>
+
+
+
+                </Container>
           </div>
         </div>
+        </center>
+      </div>
       </div>
     );
   }
