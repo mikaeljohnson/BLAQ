f658d2011a09a83c33fec1ed4a6a6541cc2ba7d7
TendieHelper <git config --global user.name>
2021-12-17 16:52:02 +0000

    Updates

---
+++ precache-manifest.4807fd45e5b91b83ce12ca376208480d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452d2579e1890037ba6a78183c14fcef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "efb57a7b488d94b83ec0",
+    "url": "./static/css/2.c3097103.chunk.css"
+  },
+  {
+    "revision": "e41b8c19528ae918f2b8",
+    "url": "./static/css/main.52911df6.chunk.css"
+  },
+  {
+    "revision": "efb57a7b488d94b83ec0",
+    "url": "./static/js/2.0ba641c0.chunk.js"
+  },
+  {
+    "revision": "3e0e93e0d402e6ae1f9aab5e6e4c1650",
+    "url": "./static/js/2.0ba641c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41b8c19528ae918f2b8",
+    "url": "./static/js/main.973ce171.chunk.js"
+  },
+  {
+    "revision": "1f0e35d68646f4e43322",
+    "url": "./static/js/runtime-main.b6be866d.js"
+  },
+  {
+    "revision": "db8a9013c0b90a784dae5a138342590f",
+    "url": "./static/media/gif.db8a9013.gif"
+  }
+]);
